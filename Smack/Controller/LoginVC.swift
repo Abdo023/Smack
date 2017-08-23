7186dc6d82a411734a8303fa0eefd4260b3ce3fa
Abdou23 <Abdou23@Abdou23-MacAir.local>
2017-08-23 17:20:54 +0200

    Login Screen UI

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by AbdelGhafour on 8/23/17.
+//  Copyright © 2017 AbdelGhafour. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+       dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by AbdelGhafour on 8/23/17.
+//  Copyright © 2017 AbdelGhafour. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
